bdb07336b921b343c470caac6eef6d79ed44a7e4
Nurbu <lamanurbu2@gmail.com>
2026-05-15 14:59:05 -0400

    Created the parent abstract Contract class with private fields, constructor and abstract methods required later to be overwritten.

---
+++ src/main/java/Contract.java
@@ -0,0 +1,62 @@
+import com.pluralsight.dealership.Vehicle;
+
+public abstract class Contract {
+
+    private String Date;
+    private String customerName;
+    private String customerEmail;
+    private Vehicle vehicleSold;
+    private double totalPrice;
+    private double monthlyPayment;
+
+    public Contract(String date, String customerName, String customerEmail, Vehicle vehicleSold, double totalPrice, double monthlyPayment) {
+        Date = date;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicleSold = vehicleSold;
+        this.totalPrice = totalPrice;
+        this.monthlyPayment = 0;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public Vehicle getVehicleSold() {
+        return vehicleSold;
+    }
+
+    public void setVehicleSold(Vehicle vehicleSold) {
+        this.vehicleSold = vehicleSold;
+    }
+
+    /**
+     * Both will get overwritten by specific contract sales or Lease
+     *
+     * @return
+     */
+    public abstract double getTotalPrice();
+
+    public abstract double getMonthlyPayment();
+
+}
